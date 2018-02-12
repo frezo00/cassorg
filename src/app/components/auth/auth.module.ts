697e0c79df7fcc38c6b9ed54d3067d3a1282b31b
Frano Rezo <frano.rezo@locastic.com>
2018-02-12 16:36:23 +0100

    Started styling on login form

--- src/app/components/auth/auth.module.ts
+++ src/app/components/auth/auth.module.ts
@@ -5,10 +5,11 @@ import { SharedModule } from '../../shared/shared.module';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { EmailConfirmationComponent } from './email-confirmation/email-confirmation.component';
+import { MaterialModule } from '../../shared/material.module';
 
 @NgModule({
   declarations: [LoginComponent, RegisterComponent, EmailConfirmationComponent],
-  imports: [CommonModule, SharedModule],
+  imports: [CommonModule, SharedModule, MaterialModule],
   exports: [LoginComponent, RegisterComponent, EmailConfirmationComponent],
   providers: []
 })
--- src/app/components/auth/login/login.component.ts
+++ src/app/components/auth/login/login.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import {
+  FormBuilder,
+  Validators,
+  FormGroup,
+  FormControl
+} from '@angular/forms';
 import { Store } from '@ngrx/store';
 import { Observable } from 'rxjs/Observable';
 
@@ -11,12 +17,47 @@ import * as AuthActions from '../store/auth.actions';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  loginForm: FormGroup;
+  email: FormControl;
+  password: FormControl;
+  showPassword: boolean;
   isAuthenicated$: Observable<boolean>;
 
-  constructor(private store: Store<fromAuth.AuthState>) {}
+  constructor(
+    private store: Store<fromAuth.AuthState>,
+    private fb: FormBuilder
+  ) {}
 
   ngOnInit() {
+    this.initForm();
     this.isAuthenicated$ = this.store.select(fromAuth.getIsAuthenticated);
+  }
+
+  initForm() {
+    this.email = new FormControl('', [Validators.required, Validators.email]);
+    this.password = new FormControl('', [
+      Validators.required,
+      Validators.minLength(6)
+    ]);
+    this.loginForm = this.fb.group({
+      email: this.email,
+      password: this.password
+    });
+    this.showPassword = false;
+  }
+
+  getEmailErrorMessage() {
+    return this.email.hasError('required')
+      ? 'You must enter a value'
+      : this.email.hasError('email') ? 'Not a valid email' : '';
+  }
+
+  getPasswordErrorMessage() {
+    return this.password.hasError('required')
+      ? 'You must enter a value'
+      : this.password.hasError('minlength')
+        ? 'Must be minimum 6 characters'
+        : '';
   }
 
   onLogin() {
